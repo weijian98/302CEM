bb94361d2035f4f0949558e1d8c9145c2b3f0420
JsHoo63 <54528462+JsHoo63@users.noreply.github.com>
2020-10-25 16:30:39 +0800

    SignUp Done

--- views/site/login.php
+++ views/site/login.php
@@ -34,14 +34,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button','style'=>'width:8%;']) ?>
+                <?= Html::a('SignUp', ['/site/signup'], ['class'=>'btn btn-primary','style'=>'width:8%;']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
 </div>
