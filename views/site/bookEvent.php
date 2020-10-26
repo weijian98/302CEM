feeb922314a2e609a500361aa546b2d873f93c31
Yuan Shen <sawyuanshen@hotmail.com>
2020-10-27 01:14:40 +0800

    update bookevent page

--- views/site/bookEvent.php
+++ views/site/bookEvent.php
@@ -4,21 +4,22 @@
 
 use yii\helpers\Url;
 use yii\helpers\Html;
+use app\models\Booking;
 $this->title = 'Book Event Details';
 ?>
 
 <div class="site-bookEvent">
 
-
-@booking=Booking::model()->findAllBySql(sql "select * from booking order by id DESC");
-
+<?php
+//$sql = 'select * from booking order by id DESC';
+$booking=Booking::find()->all();
+$print1 = [];
 if($booking != null ) {
     foreach ($booking as $row){
         $print1[] =$row;
     }
-
-    $booking1count = count($print);
-}
+    $booking1count = count($print1);
+}?>
 
 <div class="jumbotron">
         <h1>Welcome</h1>    
@@ -32,17 +33,19 @@ if($booking != null ) {
         <h2>List of Book Event Details</h2>    
  
     </div>
-<div class="<?php echo $print[$i]['id']; ?>" class="<?php if ($i != 0){echo 'panel-collapse collapse';} else {echo 'panel-collapse collapse in';}?>">
+        <?php
+        for($i =0; $i < $booking1count;$i++){?>
+<div class="<?php echo $print1[$i]['booking_id']; ?>" class="<?php if ($i != 0){echo 'panel-collapse collapse';} else {echo 'panel-collapse collapse in';}?>">
     <h2> Book Details </h2>
     <div class="panel-body">
 <?php
-echo "Booking ID: ".$print[$i];['booking_id']."<br>";
-echo "Booking Info: ".$print[$i];['booking_info_id']. "<br>";
-echo "Date of Booked Event: ".$print[$i];['booking_date']. "<br>";
-echo "Ticket ID: ".$print[$i];['ticket_id']. "<br>";
-
+echo "Booking ID: ".$print1[$i]['booking_id']."<br>";
+echo "Booking Info: ".$print1[$i]['booking_info_id']. "<br>";
+echo "Date of Booked Event: ".$print1[$i]['booking_date']. "<br>";
+echo "Ticket ID: ".$print1[$i]['ticket_id']. "<br>";
+}
 ?>
-
+    </div>
 </div>
 
-</body>
+</div>
