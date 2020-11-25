17030d40597d4557135ba6afc501cef23b56b240
chingmay <chingmay043@gmail.com>
2020-11-25 21:21:27 +0800

    Revert "update"
    This reverts commit 8bdfee3fbe4cfcb83cee13c8cb13502739aa4dae.

---
+++ views/about/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\grid\ActionColumn;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\AboutSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Abouts';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="about-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+       <!-- <?= Html::a('Create About', ['create'], ['class' => 'btn btn-success']) ?>-->
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'about:ntext',
+
+            ['class' =>ActionColumn::className(),'template'=>'{view} {update}'],
+        ],
+    ]); ?>
+
+
+</div>
